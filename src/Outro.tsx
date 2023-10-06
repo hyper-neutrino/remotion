f56cce0dc0fb8bad46060d50c0b6b369b4f93e25
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-06 10:26:21 -0400

    put fadeout into outro directly

--- src/Outro.tsx
+++ src/Outro.tsx
@@ -1,9 +1,10 @@
 import { loadFont } from "@remotion/google-fonts/ShareTech";
 import { zColor } from "@remotion/zod-types";
 import React from "react";
-import { AbsoluteFill, interpolate, spring, useCurrentFrame, useVideoConfig } from "remotion";
+import { AbsoluteFill, interpolate, Sequence, spring, useCurrentFrame, useVideoConfig } from "remotion";
 import { z } from "zod";
 import { Subscribe } from "./cta/Subscribe";
+import { FadeTo } from "./FadeTo";
 import { Pane } from "./outro/Pane";
 
 const { fontFamily } = loadFont();
@@ -37,43 +38,50 @@ export const Outro: React.FC<z.infer<typeof schema>> = ({
     const w = (config.height * 0.38 * 16) / 9;
 
     return (
-        <AbsoluteFill style={{ backgroundColor }}>
-            <Pane x={config.width - w - config.height * 0.08} y={config.height * 0.08} w={w} h={config.height * 0.38} backgroundColor={paneColor} />
-            <Pane x={config.width - w - config.height * 0.08} y={config.height * 0.54} w={w} h={config.height * 0.38} backgroundColor={paneColor} />
-            <AbsoluteFill
-                style={{
-                    textAlign: "center",
-                    width: config.width - w - config.height * 0.08,
-                    top: config.height * 0.25,
-                    fontSize: config.height * 0.1,
-                    fontFamily,
-                    color: titleColor,
-                }}
-            >
-                <div style={{ opacity: interpolate(frame, [60, 90], [0, 1]) }}>{title}</div>
-                <div
-                    style={{
-                        borderBottom: `2px solid ${titleUnderlineColor}`,
-                        translate: `${spring({ frame, fps: 60, from: -100, to: 0, durationInFrames: 60, delay: 60, config: { damping: 12 } })}%`,
-                    }}
-                />
-            </AbsoluteFill>
-            <AbsoluteFill style={{ top: config.height * 0.85, left: config.width * 0.05 }}>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        gap: "0.4em",
-                        fontFamily,
-                        fontSize: config.height * 0.05,
-                        color: titleColor,
-                    }}
-                >
-                    <Subscribe {...{ subscribeColor, subscribeBackgroundColor, subscribeColorFill, subscribeBackgroundColorFill }} />
-                    <div>for more!</div>
-                </div>
-            </AbsoluteFill>
-        </AbsoluteFill>
+        <>
+            <Sequence durationInFrames={60}>
+                <FadeTo {...{ backgroundColor }} />
+            </Sequence>
+            <Sequence from={60}>
+                <AbsoluteFill style={{ backgroundColor }}>
+                    <Pane x={config.width - w - config.height * 0.08} y={config.height * 0.08} w={w} h={config.height * 0.38} backgroundColor={paneColor} />
+                    <Pane x={config.width - w - config.height * 0.08} y={config.height * 0.54} w={w} h={config.height * 0.38} backgroundColor={paneColor} />
+                    <AbsoluteFill
+                        style={{
+                            textAlign: "center",
+                            width: config.width - w - config.height * 0.08,
+                            top: config.height * 0.25,
+                            fontSize: config.height * 0.1,
+                            fontFamily,
+                            color: titleColor,
+                        }}
+                    >
+                        <div style={{ opacity: interpolate(frame, [60, 90], [0, 1]) }}>{title}</div>
+                        <div
+                            style={{
+                                borderBottom: `2px solid ${titleUnderlineColor}`,
+                                translate: `${spring({ frame, fps: 60, from: -100, to: 0, durationInFrames: 60, delay: 60, config: { damping: 12 } })}%`,
+                            }}
+                        />
+                    </AbsoluteFill>
+                    <AbsoluteFill style={{ top: config.height * 0.85, left: config.width * 0.05 }}>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                alignItems: "center",
+                                gap: "0.4em",
+                                fontFamily,
+                                fontSize: config.height * 0.05,
+                                color: titleColor,
+                            }}
+                        >
+                            <Subscribe {...{ subscribeColor, subscribeBackgroundColor, subscribeColorFill, subscribeBackgroundColorFill }} />
+                            <div>for more!</div>
+                        </div>
+                    </AbsoluteFill>
+                </AbsoluteFill>
+            </Sequence>
+        </>
     );
 };
