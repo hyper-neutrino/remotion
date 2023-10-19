e03e049744676d087b59162e413cdf62af5b9a31
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-19 14:32:16 -0400

    use Series

--- src/videos/DeepDiveBezierCurves/index.tsx
+++ src/videos/DeepDiveBezierCurves/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AbsoluteFill, interpolate, Sequence, useCurrentFrame, useVideoConfig } from "remotion";
+import { AbsoluteFill, interpolate, Series, useCurrentFrame, useVideoConfig } from "remotion";
 import { Intro } from "../../components/Intro";
 
 export const duration = 1000;
@@ -10,17 +10,17 @@ export const DeepDiveBezierCurves: React.FC = () => {
 
     return (
         <AbsoluteFill style={{ backgroundColor: "#111214" }}>
-            <Sequence durationInFrames={330}>
+            <Series.Sequence durationInFrames={330}>
                 <Intro subtitle="Deep Dive: Bezier Curves" />
-            </Sequence>
-            <Sequence from={330} durationInFrames={180}>
+            </Series.Sequence>
+            <Series.Sequence durationInFrames={180}>
                 <svg
                     viewBox={`0 0 ${w} ${h}`}
                     style={{ strokeWidth: w * 0.01, stroke: "white", fill: "transparent", opacity: interpolate(frame, [330, 380, 480, 510], [0, 1, 1, 0]) }}
                 >
                     <path d={`M ${w / 3} ${h / 3} C ${w} ${(h * 2) / 3}, ${0} ${(h * 2) / 3}, ${(w * 2) / 3} ${h / 3}`} />
                 </svg>
-            </Sequence>
+            </Series.Sequence>
         </AbsoluteFill>
     );
 };
